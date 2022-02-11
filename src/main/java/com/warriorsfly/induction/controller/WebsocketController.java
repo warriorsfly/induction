652d1aed866de88cccb4b3ca0657df8a6fca812c
walker <warriorsfly@gmail.com>
2022-02-11 23:39:37 +0800

    remove MedicalEventConsumer.java,make WebsocketController implements StreamListener

--- src/main/java/com/warriorsfly/induction/controller/WebsocketController.java
+++ src/main/java/com/warriorsfly/induction/controller/WebsocketController.java
@@ -1,28 +1,35 @@
 package com.warriorsfly.induction.controller;
 
+import com.warriorsfly.induction.domain.Message;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.stream.StreamListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 import java.security.Principal;
 
 @Controller
-public class WebsocketController {
+public class WebsocketController implements StreamListener<String, ObjectRecord<String, Message>> {
+    @Override
+    public void onMessage(ObjectRecord<String, Message> message) {
+//        if(message instanceof )
+        
+    }
 
-//    @MessageMapping("/hello")
-//    @SendTo("/topic/greetings")
-//    public Greeting greeting(HelloMessage message) throws Exception {
-//        Thread.sleep(1000); // simulated delay
-//        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-//    }
-//
-//    @MessageMapping("/furion")
-//    @SendToUser("/topic/message")
-//    public Message push(@Payload Message message, Principal user){
-//        return message;
-//    }
+    @MessageMapping("/broadcast")
+    @SendTo("/topic/broadcast")
+    public Message greeting(Message message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return message;
+    }
+
+    @MessageMapping("/chat")
+    @SendToUser("/topic/chat")
+    public Message push(@Payload Message message, Principal user){
+        return message;
+    }
 
 }
