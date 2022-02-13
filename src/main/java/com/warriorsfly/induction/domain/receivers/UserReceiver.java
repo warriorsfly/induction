f05d80ec2baba4fe24b574c0a3dd90363484ce86
walker <warriorsfly@gmail.com>
2022-02-13 23:50:56 +0800

    remove receivers

--- src/main/java/com/warriorsfly/induction/domain/receivers/UserReceiver.java
+++
@@ -1,6 +0,0 @@
-package com.warriorsfly.induction.domain.receivers;
-
-import com.warriorsfly.induction.domain.MessageReceiver;
-import com.warriorsfly.induction.domain.User;
-
-public class UserReceiver extends MessageReceiver<User> { }
